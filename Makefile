44e3c2fc92d6b67b0570ca064985d62bd65d3226
Clement Fleury <clement.fleury@pm.me>
2021-07-30 17:51:59 +0200

    process data

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-.PHONY: env clean-env deps lint test clean-test data clean-data clean-notebook clean
+.PHONY: env clean-env deps lint format mypy test clean-test data clean-data clean-notebook clean
 
 help:
 	@echo "           env - Create local environment with Conda or venv : $(PWD)/env"
 	@echo "     clean-env - Delete environment"
 	@echo "          deps - Install Python Dependencies with Mamba, Conda or pip"
 	@echo "          lint - Lint using flake8"
+	@echo "        format - Format using black"
+	@echo "          mypy - Type check using mypy"
 	@echo "          test - Test and produce coverage report using pytest"
 	@echo "    clean-test - Delete test files"
 	@echo "          data - Download and process dataset"
@@ -73,6 +75,14 @@ endif
 lint:
 	flake8 src/ tests/
 
+## Format using black
+format:
+	black src/ tests/
+
+## Type check using mypy
+mypy:
+	mypy src/ tests/
+
 ## Test and produce coverage report using pytest
 test:
 	pytest --cov=src/ --cov-report=xml:cobertura.xml tests/
@@ -82,7 +92,7 @@ clean-test:
 	rm -rf .pytest_cache .coverage cobertura.xml
 
 ## Download and process dataset
-data: 
+data:
 	python src/data/make_dataset.py data/raw data/processed
 
 ## Delete data files
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,29 +7,148 @@ import requests
 import zipfile
 import io
 
+import pandas as pd
 
-def download_extract_zip(zip_file_url: str, raw_data_path: str) -> None:
-    """Downloads a zip file from a given url
-    and saves its content files to local path.
+from typing import Final
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+DATA_FILES: Final[tuple[str, ...]] = (
+    str(os.getenv("APPLICATION_TEST_FILE_NAME")),
+    str(os.getenv("APPLICATION_TRAIN_FILE_NAME")),
+    str(os.getenv("BUREAU_BALANCE_FILE_NAME")),
+    str(os.getenv("BUREAU_FILE_NAME")),
+    str(os.getenv("CREDIT_CARD_BALANCE_FILE_NAME")),
+    str(os.getenv("INSTALLMENTS_PAYMENTS_FILE_NAME")),
+    str(os.getenv("POS_CASH_BALANCE_FILE_NAME")),
+    str(os.getenv("PREVIOUS_APPLICATION_FILE_NAME")),
+    str(os.getenv("SAMPLE_SUBMISSION_FILE_NAME")),
+)
+
+
+def download_extract_zip(zip_file_url: str, content_path: str) -> None:
+    """Download Zip from url and extract content files to local path.
+
+    - Check if content files already exist.
+        - If they all exist, return.
+        - If not, download zip file and extract content files.
+
+    Args:
+        zip_file_url: Url of zip file to download.
+        content_path: Path to extract zip contents to.
+
+    Returns:
+        None
     """
+
+    # We must NOT download and extract zip file by default.
+    must_download: bool = False
+
+    for file in DATA_FILES:
+        # Check if content files exist
+        file_path = os.path.join(content_path, file)
+        if not os.path.exists(file_path):
+            # If at least one file does not exist, we must download zip file
+            must_download = True
+            break
+
+    # If all files already exist, return
+    if not must_download:
+        return
+
+    # Download zip file
     r = requests.get(zip_file_url)
+    if r.status_code != 200:
+        raise ValueError(f"Could not download {zip_file_url}")
+
+    # Check if zip file is OK
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall(raw_data_path)
+    if z.testzip() is not None:
+        raise ValueError(f"Could not extract {zip_file_url}")
+
+    # Check if content path exists
+    if not os.path.exists(content_path):
+        os.makedirs(content_path)
+
+    # Extract files from zip
+    z.extractall(content_path)
 
 
-def main():
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def process_raw_files(raw_path: str, processed_path: str) -> None:
+    """Read raw CSV files and save processed data as CSV.
+
+    Args:
+        raw_path: Path to raw data files.
+        processed_path: Path to save processed data files.
+
+    Returns:
+        None
     """
+    for file in DATA_FILES:
+        # Check if processed file already exists
+        processed_file_path = os.path.join(processed_path, file)
+        if os.path.exists(processed_file_path):
+            continue
+
+        # Check if raw file exists
+        raw_file_path = os.path.join(raw_path, file)
+        if not os.path.exists(raw_file_path):
+            raise ValueError(f"Could not find {raw_file_path}")
+
+        # Read raw data
+        df = pd.read_csv(raw_file_path)
+
+        # Check if processed path exists
+        if not os.path.exists(processed_path):
+            os.makedirs(processed_path)
+
+        # Write data to CSV
+        df.to_csv(processed_file_path, index=False)
+
+
+def main() -> None:
+    """Run data processing scripts.
+
+    - Fix absolute files paths.
+    - Download and extract data.
+    - Process raw data into CSV.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    if os.getenv("ZIP_FILE_URL") is None:
+        raise ValueError("ZIP_FILE_URL is not set")
+
+    if os.getenv("RAW_DATA_PATH") is None:
+        raise ValueError("RAW_DATA_PATH is not set")
+
+    if os.getenv("PROCESSED_DATA_PATH") is None:
+        raise ValueError("PROCESSED_DATA_PATH is not set")
+
+    # Check absolute paths
+    current_dir = os.path.dirname(__file__)
+    src_path = os.path.abspath(os.path.join(current_dir, os.pardir))
+    project_path = os.path.abspath(os.path.join(src_path, os.pardir))
+    raw_data_path = os.path.join(project_path, str(os.getenv("RAW_DATA_PATH")))
+    processed_data_path = os.path.join(
+        project_path, str(os.getenv("PROCESSED_DATA_PATH"))
+    )
+
+    # Download zip from url and extract files to raw data path.
     download_extract_zip(
-        os.getenv('ZIP_FILE_URL'),
-        os.getenv('RAW_DATA_PATH')
+        os.path.abspath(str(os.getenv("ZIP_FILE_URL"))), raw_data_path,
+    )
+
+    # Read data into CSV
+    process_raw_files(
+        raw_data_path, processed_data_path,
     )
 
 
-if __name__ == '__main__':
-    # Load environment variables from .env file
-    load_dotenv(find_dotenv())
-
+if __name__ == "__main__":
     # Run main function
     main()
