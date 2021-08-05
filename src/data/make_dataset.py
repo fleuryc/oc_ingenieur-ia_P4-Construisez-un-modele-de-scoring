a696f2afdf744229f370c1789118f3286d984924
Clement Fleury <clement.fleury@pm.me>
2021-08-05 19:13:25 +0200

    merge CSVs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -14,7 +14,7 @@ from typing import Final
 # Load environment variables from .env file
 load_dotenv(find_dotenv())
 
-DATA_FILES: Final[tuple[str, ...]] = (
+DATA_FILES: Final[tuple[str]] = (
     str(os.getenv("APPLICATION_TEST_FILE_NAME")),
     str(os.getenv("APPLICATION_TRAIN_FILE_NAME")),
     str(os.getenv("BUREAU_BALANCE_FILE_NAME")),
@@ -24,6 +24,28 @@ DATA_FILES: Final[tuple[str, ...]] = (
     str(os.getenv("POS_CASH_BALANCE_FILE_NAME")),
     str(os.getenv("PREVIOUS_APPLICATION_FILE_NAME")),
     str(os.getenv("SAMPLE_SUBMISSION_FILE_NAME")),
+)
+
+DATA_FILES_RELATIONS: Final[tuple[tuple[str, tuple[str]]]] = (
+    # (file, (foreign_keys))
+    (str(os.getenv("BUREAU_FILE_NAME")), ("SK_ID_CURR"),),
+    (str(os.getenv("BUREAU_BALANCE_FILE_NAME")), ("SK_ID_BUREAU"),),
+    (
+        str(os.getenv("POS_CASH_BALANCE_FILE_NAME")),
+        ("SK_ID_CURR", "MONTHS_BALANCE"),
+    ),
+    (
+        str(os.getenv("CREDIT_CARD_BALANCE_FILE_NAME")),
+        ("SK_ID_PREV", "SK_ID_CURR", "MONTHS_BALANCE"),
+    ),
+    (
+        str(os.getenv("PREVIOUS_APPLICATION_FILE_NAME")),
+        ("SK_ID_PREV", "SK_ID_CURR"),
+    ),
+    (
+        str(os.getenv("INSTALLMENTS_PAYMENTS_FILE_NAME")),
+        ("SK_ID_PREV", "SK_ID_CURR"),
+    ),
 )
 
 
@@ -99,12 +121,56 @@ def process_raw_files(raw_path: str, processed_path: str) -> None:
         # Read raw data
         df = pd.read_csv(raw_file_path)
 
+        # ! PROCESS DATA HERE
+
         # Check if processed path exists
         if not os.path.exists(processed_path):
             os.makedirs(processed_path)
 
         # Write data to CSV
         df.to_csv(processed_file_path, index=False)
+
+
+def merge_csv_files(processed_data_path: str, merged_file_path: str) -> None:
+    """Merge CSV files.
+
+    Args:
+        processed_data_path: Path to processed data files.
+        merged_file_path: Path to save merged CSV file.
+
+    Returns:
+        None
+    """
+    # Check if merged file already exists
+    if os.path.exists(merged_file_path):
+        return
+
+    # Check if application file exists
+    application_file_path = os.path.join(
+        processed_data_path, str(os.getenv("APPLICATION_TEST_FILE_NAME"))
+    )
+    if not os.path.exists(application_file_path):
+        raise ValueError(f"Could not find {application_file_path}")
+
+    # Read application data
+    merged_df = pd.read_csv(application_file_path)
+    merged_df.to_csv(merged_file_path)
+    del merged_df
+
+    for relation in DATA_FILES_RELATIONS:
+        print(relation)
+
+        # Check if relation file exists
+        relation_file_path = os.path.join(processed_data_path, relation[0])
+        if not os.path.exists(relation_file_path):
+            raise ValueError(f"Could not find {relation_file_path}")
+
+        merged_df = pd.read_csv(merged_file_path)
+        relation_df = pd.read_csv(relation_file_path)
+
+        merged_df = pd.merge(merged_df, relation_df, how="left", on=relation[1])
+        merged_df.to_csv(merged_file_path)
+        del merged_df, relation_df
 
 
 def main() -> None:
@@ -146,6 +212,11 @@ def main() -> None:
     # Read data into CSV
     process_raw_files(
         raw_data_path, processed_data_path,
+    )
+
+    # Merge CSV files
+    merge_csv_files(
+        processed_data_path, os.path.join(processed_data_path, "merged.csv"),
     )
 
 
